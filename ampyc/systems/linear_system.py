774962ae751980db12de2e010d4f934003f0fcd2
Pierre Ballif <pierre@ballif.eu>
2025-11-25 17:59:20 +0100

    LinearSystem handles noise in both u and x

--- ampyc/systems/linear_system.py
+++ ampyc/systems/linear_system.py
@@ -22,10 +22,13 @@ class LinearSystemParams:
     B: np.ndarray
     C: np.ndarray
     D: np.ndarray | Literal[0] = 0
+    B_w: np.ndarray | Literal[0] = 0
+    D_w: np.ndarray | Literal[0] = 0
 
     n: int = field(init=False)
     m: int = field(init=False)
     p: int = field(init=False)
+    q: int | None = field(init=False)
 
     A_u: np.ndarray | None = None
     b_u: np.ndarray | None = None
@@ -43,12 +46,27 @@ class LinearSystemParams:
         assert self.A.shape == (self.n, self.n)
         assert self.B.shape == (self.n, self.m)
         assert self.C.shape == (self.p, self.n)
-        if self.D == 0:
+        if isinstance(self.D, int) and self.D == 0:
             self.D = np.zeros((self.p, self.m))
         else:
             assert self.D.shape == (self.p, self.m)
+
+        B_w_zero = isinstance(self.B_w, int) and self.B_w == 0
+        D_w_zero = isinstance(self.D_w, int) and self.D_w == 0
+        if B_w_zero and D_w_zero:
+            self.q = 0
+        else:
+            self.q = self.B_w.shape[1] if not B_w_zero else self.D_w.shape[1]
+            if B_w_zero:
+                self.B_w = np.zeros((self.n, self.q))
+            else:
+                assert self.B_w.shape == (self.n, self.q)
+            if D_w_zero:
+                self.D_w = np.zeros((self.p, self.q))
+            else:
+                assert self.D_w.shape == (self.p, self.q), f"Expected D_w to have shape {self.p, self.q} but has shape {self.D_w.shape}"
         if self.noise_generator is None:
-            self.noise_generator = ZeroNoise(self.n)
+            self.noise_generator = ZeroNoise(self.q)
 
 
 class LinearSystem(SystemBase):
@@ -72,14 +90,30 @@ class LinearSystem(SystemBase):
         self.B = params.B
         self.C = params.C
         self.D = params.D
+        self.B_w = params.B_w
+        self.D_w = params.D_w
 
-    def f(self, x, u):
+    def f(self, x: np.ndarray, u: np.ndarray):
+        """Nominal dynamics."""
+        return (
+            self.A @ x.reshape(self.n, 1) +
+            self.B @ u.reshape(self.m, 1)
+        )
+
+    def h(self, x: np.ndarray, u: np.ndarray):
+        return (
+            self.C @ x.reshape(self.n, 1) +
+            self.D @ u.reshape(self.m, 1)
+        )
+
+    def step(self, x, u):
         self._check_x_shape(x)  # make sure x is n dimensional
         self._check_u_shape(u)  # make sure u is m dimensional
-        return self.A @ x.reshape(self.n, 1) + self.B @ u.reshape(self.m, 1)
-
-    def h(self, x, u):
-        self._check_x_shape(x)  # make sure x is n dimensional
-        self._check_u_shape(u)  # make sure u is m dimensional
-        return self.C @ x.reshape(self.n, 1) + self.D @ u.reshape(self.m, 1)
-
+        if self.q != 0:
+            w = self.noise_generator._generate()
+            x_new = self.f(x, u) + self.B_w @ w
+            y = self.h(x_new, u) + self.D_w @ w
+        else:
+            x_new = self.f(x, u)
+            y = self.h(x, u)
+        return x_new, y
