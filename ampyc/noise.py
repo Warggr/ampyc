6469b4add42e21f2995ed37f8effb768e2308d1c
Pierre Ballif <pierre@ballif.eu>
2026-02-23 11:10:42 +0100

    do not modify W.V in PolytopeNoise

--- ampyc/noise.py
+++ ampyc/noise.py
@@ -9,6 +9,7 @@
 
 from abc import ABC, abstractmethod
 import numpy as np
+from polytope import extreme
 
 from ampyc.utils import Polytope, qhull
 
@@ -98,15 +99,10 @@ class PolytopeVerticesNoise(NoiseBase):
 
     def __init__(self, W: Polytope, seed: int | None = None) -> None:
         assert seed is None or seed >= 0
-        self.W = W
-        self.V = W.V
+        np.random.seed(0)  # get deterministic behavior of `extreme`
+        # create new polytope to force re-computation
+        self.V = extreme(Polytope(W.A, W.b))
         self.rng = np.random.default_rng(seed)
-
-    def seed(self, seed: int | None = None):
-        super().seed(seed)
-        np.random.seed(seed)
-        self.W.vertices = None  # trigger new Vrep computation
-        self.V = self.W.Vrep()
 
     def _generate(self, N: int | None = None) -> np.ndarray:
         if N is None:
@@ -123,15 +119,10 @@ class PolytopeNoise(NoiseBase):
 
     def __init__(self, W: Polytope, seed: int | None = None) -> None:
         assert seed is None or seed >= 0
-        self.W = W
-        self.V = W.V
+        np.random.seed(0)  # get deterministic behavior of `extreme`
+        # create new polytope to force re-computation
+        self.V = extreme(Polytope(W.A, W.b))
         self.rng = np.random.default_rng(seed)
-
-    def seed(self, seed: int | None = None):
-        super().seed(seed)
-        np.random.seed(seed)
-        self.W.vertices = None  # trigger new Vrep computation
-        self.V = self.W.Vrep()
 
     def _generate(self, N: int | None = None) -> np.ndarray:
         """Based on implementation for randomPoint() in MPT"""
